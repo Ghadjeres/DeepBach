4a4c147e6a817aff34c9b0ff3df87e98f36a788f
gaetan <hadjeres.g@gmail.com>
2017-01-05 13:32:51 +0100

    remove old models

--- deepBach.py
+++ deepBach.py
@@ -35,12 +35,12 @@ def generation(model_base_name, models, timesteps, melody=None, chorale_metas=No
     # Test by generating a sequence
 
     if parallel:
-        seq = parallelGibbs(models=models, model_base_name=model_base_name,
-                            melody=melody, chorale_metas=chorale_metas, timesteps=timesteps,
-                            num_iterations=num_iterations, sequence_length=sequence_length,
-                            temperature=temperature,
-                            initial_seq=initial_seq, batch_size_per_voice=batch_size_per_voice,
-                            parallel_updates=True, pickled_dataset=pickled_dataset, metadatas=metadatas)
+        seq = parallel_gibbs(models=models, model_base_name=model_base_name,
+                             melody=melody, chorale_metas=chorale_metas, timesteps=timesteps,
+                             num_iterations=num_iterations, sequence_length=sequence_length,
+                             temperature=temperature,
+                             initial_seq=initial_seq, batch_size_per_voice=batch_size_per_voice,
+                             parallel_updates=True, pickled_dataset=pickled_dataset, metadatas=metadatas)
 
     else:
         # todo refactor
@@ -71,84 +71,7 @@ def generation(model_base_name, models, timesteps, melody=None, chorale_metas=No
     return seq
 
 
-def fastBach(num_features_lr, num_features_c, num_pitches, num_units_lstm=[200],
-             num_dense=200, batch_size=16, timesteps=16):
-    """
-
-    :param num_features_lr: size of left or right features vectors
-    :param num_features_c: size of central features vectors
-    :param num_pitches: size of output
-    :param num_units_lstm: list of lstm layer sizes
-    :param num_dense:
-    :return:
-    """
-    left_features = Input(shape=(timesteps, num_features_lr), name='left_features')
-    right_features = Input(shape=(timesteps, num_features_lr), name='right_features')
-    central_features = Input(shape=(num_features_c,), name='central_features')
-    beat = Input(shape=(BEAT_SIZE,), name='beat')
-    beats_right = Input(shape=(timesteps, BEAT_SIZE), name='beats_right')
-    beats_left = Input(shape=(timesteps, BEAT_SIZE), name='beats_left')
-    fermatas_left = Input(shape=(timesteps, BITS_FERMATA), name='fermatas_left')
-    fermatas_right = Input(shape=(timesteps, BITS_FERMATA), name='fermatas_right')
-    central_fermata = Input(shape=(BITS_FERMATA,), name='central_fermata')
-
-    # embedding layer for left and right
-    embedding_left = Dense(input_dim=num_features_lr + BEAT_SIZE + BITS_FERMATA,
-                           output_dim=num_dense, name='embedding_left')
-    embedding_right = Dense(input_dim=num_features_lr + BEAT_SIZE + BITS_FERMATA,
-                            output_dim=num_dense, name='embedding_right')
-
-    predictions_left = TimeDistributed(embedding_left)(merge((left_features,
-                                                              beats_left,
-                                                              fermatas_left),
-                                                             mode='concat'))
-    predictions_right = TimeDistributed(embedding_right)(merge((right_features,
-                                                                beats_right,
-                                                                fermatas_right),
-                                                               mode='concat'))
-
-    predictions_center = merge((central_features, beat,
-                                central_fermata), mode='concat')
-
-    predictions_center = Dense(num_dense, activation='relu')(predictions_center)
-    predictions_center = Dense(num_dense, activation='relu')(predictions_center)
-
-    return_sequences = True
-    for k, stack_index in enumerate(range(len(num_units_lstm))):
-        if k == len(num_units_lstm) - 1:
-            return_sequences = False
-        predictions_left = FastWeights(num_units_lstm[stack_index],
-                                       return_sequences=return_sequences,
-                                       name='lstm_left_' + str(stack_index),
-                                       unroll=True, consume_less='gpu'
-                                       )(predictions_left)
-        predictions_right = FastWeights(num_units_lstm[stack_index],
-                                        return_sequences=return_sequences,
-                                        name='lstm_right_' + str(stack_index),
-                                        unroll=True,
-                                        consume_less='gpu'
-
-                                        )(predictions_right)
-
-    predictions = merge((predictions_left, predictions_center, predictions_right),
-                        mode='concat')
-    predictions = Dense(num_dense, activation='relu')(predictions)
-    pitch_prediction = Dense(num_pitches, activation='softmax',
-                             name='pitch_prediction')(predictions)
-
-    model = Model(input=[left_features, central_features, right_features,
-                         beat, beats_left, beats_right,
-                         fermatas_left, fermatas_right, central_fermata],
-                  output=pitch_prediction)
-
-    model.compile(optimizer='adam',
-                  loss={'pitch_prediction': 'categorical_crossentropy'},
-                  metrics=['accuracy'])
-    model.summary()
-    return model
-
-
-def deepBach(num_features_lr, num_features_c, num_pitches, num_units_lstm=[200],
+def deepBach(num_features_lr, num_features_c, num_pitches, num_features_meta, num_units_lstm=[200],
              num_dense=200, timesteps=16):
     """
 
@@ -159,33 +82,29 @@ def deepBach(num_features_lr, num_features_c, num_pitches, num_units_lstm=[200],
     :param num_dense:
     :return:
     """
+    # input
     left_features = Input(shape=(timesteps, num_features_lr), name='left_features')
     right_features = Input(shape=(timesteps, num_features_lr), name='right_features')
     central_features = Input(shape=(num_features_c,), name='central_features')
-    beat = Input(shape=(BEAT_SIZE,), name='beat')
-    beats_right = Input(shape=(timesteps, BEAT_SIZE), name='beats_right')
-    beats_left = Input(shape=(timesteps, BEAT_SIZE), name='beats_left')
-    fermatas_left = Input(shape=(timesteps, BITS_FERMATA), name='fermatas_left')
-    fermatas_right = Input(shape=(timesteps, BITS_FERMATA), name='fermatas_right')
-    central_fermata = Input(shape=(BITS_FERMATA,), name='central_fermata')
+    # input metadatas
+    left_metas = Input(shape=(timesteps, num_features_meta), name='left_metas')
+    right_metas = Input(shape=(timesteps, num_features_meta), name='right_metas')
+    central_metas = Input(shape=(num_features_meta,), name='central_metas')
 
     # embedding layer for left and right
-    embedding_left = Dense(input_dim=num_features_lr + BEAT_SIZE + BITS_FERMATA,
+    embedding_left = Dense(input_dim=num_features_lr + num_features_meta,
                            output_dim=num_dense, name='embedding_left')
-    embedding_right = Dense(input_dim=num_features_lr + BEAT_SIZE + BITS_FERMATA,
+    embedding_right = Dense(input_dim=num_features_lr + num_features_meta,
                             output_dim=num_dense, name='embedding_right')
 
     predictions_left = TimeDistributed(embedding_left)(merge((left_features,
-                                                              beats_left,
-                                                              fermatas_left),
+                                                              left_metas),
                                                              mode='concat'))
     predictions_right = TimeDistributed(embedding_right)(merge((right_features,
-                                                                beats_right,
-                                                                fermatas_right),
+                                                                right_metas),
                                                                mode='concat'))
 
-    predictions_center = merge((central_features, beat,
-                                central_fermata), mode='concat')
+    predictions_center = merge((central_features, central_metas), mode='concat')
 
     predictions_center = Dense(num_dense, activation='relu')(predictions_center)
     predictions_center = Dense(num_dense, activation='relu')(predictions_center)
@@ -210,8 +129,8 @@ def deepBach(num_features_lr, num_features_c, num_pitches, num_units_lstm=[200],
                              name='pitch_prediction')(predictions)
 
     model = Model(input=[left_features, central_features, right_features,
-                         beat, beats_left, beats_right,
-                         fermatas_left, fermatas_right, central_fermata],
+                         left_metas, central_metas, right_metas
+                         ],
                   output=pitch_prediction)
 
     model.compile(optimizer='adam',
@@ -295,7 +214,6 @@ def skip(num_features_lr, num_features_c, num_features_meta, num_pitches, num_un
                                  return_sequences=return_sequences,
                                  name='lstm_right_' + str(stack_index)
                                  )(predictions_right)
-        # todo add relu
 
     # retain only last input for skip connections
     predictions_left_old = Lambda(lambda t: t[:, -1, :],
@@ -304,6 +222,7 @@ def skip(num_features_lr, num_features_c, num_features_meta, num_pitches, num_un
     predictions_right_old = Lambda(lambda t: t[:, -1, :],
                                    output_shape=lambda input_shape: (input_shape[0], input_shape[-1],)
                                    )(predictions_right_old)
+    # concat or sum
     predictions_left = merge([Activation('relu')(predictions_left), predictions_left_old], mode='concat')
     predictions_right = merge([Activation('relu')(predictions_right), predictions_right_old], mode='concat')
 
@@ -316,102 +235,6 @@ def skip(num_features_lr, num_features_c, num_features_meta, num_pitches, num_un
     model = Model(input=[left_features, central_features, right_features,
                          beat, beats_left, beats_right,
                          left_metas, right_metas, central_metas],
-                  output=pitch_prediction)
-
-    model.compile(optimizer='adam',
-                  loss={'pitch_prediction': 'categorical_crossentropy'},
-                  metrics=['accuracy'])
-    model.summary()
-    return model
-
-
-def skip_nofermata(num_features_lr, num_features_c, num_pitches, num_units_lstm=[200],
-                   num_dense=200, timesteps=16):
-    """
-
-    :param num_features_lr: size of left or right features vectors
-    :param num_features_c: size of central features vectors
-    :param num_pitches: size of output
-    :param num_units_lstm: list of lstm layer sizes
-    :param num_dense:
-    :return:
-    """
-    left_features = Input(shape=(timesteps, num_features_lr), name='left_features')
-    right_features = Input(shape=(timesteps, num_features_lr), name='right_features')
-    central_features = Input(shape=(num_features_c,), name='central_features')
-    beat = Input(shape=(BEAT_SIZE,), name='beat')
-    beats_right = Input(shape=(timesteps, BEAT_SIZE), name='beats_right')
-    beats_left = Input(shape=(timesteps, BEAT_SIZE), name='beats_left')
-
-    # embedding layer for left and right
-    embedding_left = Dense(input_dim=num_features_lr + BEAT_SIZE,
-                           output_dim=num_dense, name='embedding_left')
-    embedding_right = Dense(input_dim=num_features_lr + BEAT_SIZE,
-                            output_dim=num_dense, name='embedding_right')
-
-    predictions_left = TimeDistributed(embedding_left)(merge((left_features,
-                                                              beats_left),
-                                                             mode='concat'))
-    predictions_right = TimeDistributed(embedding_right)(merge((right_features,
-                                                                beats_right),
-                                                               mode='concat'))
-
-    predictions_center = merge((central_features, beat), mode='concat')
-
-    predictions_center = Dense(num_dense, activation='relu')(predictions_center)
-    predictions_center = Dense(num_dense, activation='relu')(predictions_center)
-
-    # dropout
-    predictions_left = Dropout(0.2)(predictions_left)
-    predictions_right = Dropout(0.2)(predictions_right)
-    predictions_center = Dropout(0.2)(predictions_center)
-
-    return_sequences = True
-    for k, stack_index in enumerate(range(len(num_units_lstm))):
-        if k == len(num_units_lstm) - 1:
-            return_sequences = False
-
-        if k > 0:
-            # todo it merges all inputs, not only the previous one
-            predictions_left_tmp = merge([Activation('relu')(predictions_left), predictions_left_old], mode='concat')
-            predictions_right_tmp = merge([Activation('relu')(predictions_right), predictions_right_old], mode='concat')
-        else:
-            predictions_left_tmp = predictions_left
-            predictions_right_tmp = predictions_right
-
-        predictions_left_old = predictions_left
-        predictions_right_old = predictions_right
-        predictions_left = predictions_left_tmp
-        predictions_right = predictions_right_tmp
-
-        predictions_left = LSTM(num_units_lstm[stack_index],
-                                return_sequences=return_sequences,
-                                name='lstm_left_' + str(stack_index)
-                                )(predictions_left)
-
-        predictions_right = LSTM(num_units_lstm[stack_index],
-                                 return_sequences=return_sequences,
-                                 name='lstm_right_' + str(stack_index)
-                                 )(predictions_right)
-
-    # retain only last input for skip connections
-    predictions_left_old = Lambda(lambda t: t[:, -1, :],
-                                  output_shape=lambda input_shape: (input_shape[0], input_shape[-1])
-                                  )(predictions_left_old)
-    predictions_right_old = Lambda(lambda t: t[:, -1, :],
-                                   output_shape=lambda input_shape: (input_shape[0], input_shape[-1],)
-                                   )(predictions_right_old)
-    predictions_left = merge([Activation('relu')(predictions_left), predictions_left_old], mode='concat')
-    predictions_right = merge([Activation('relu')(predictions_right), predictions_right_old], mode='concat')
-
-    predictions = merge([predictions_left, predictions_center, predictions_right],
-                        mode='concat')
-    predictions = Dense(num_dense, activation='relu')(predictions)
-    pitch_prediction = Dense(num_pitches, activation='softmax',
-                             name='pitch_prediction')(predictions)
-
-    model = Model(input=[left_features, central_features, right_features,
-                         beat, beats_left, beats_right],
                   output=pitch_prediction)
 
     model.compile(optimizer='adam',
@@ -537,11 +360,11 @@ def skip_nofermata(num_features_lr, num_features_c, num_pitches, num_units_lstm=
 #     return seq[timesteps:-timesteps, :]
 
 
-def parallelGibbs(models=None, melody=None, chorale_metas=None, sequence_length=50, num_iterations=1000,
-                  timesteps=16,
-                  model_base_name='models/raw_dataset/tmp/',
-                  temperature=1., initial_seq=None, batch_size_per_voice=16, parallel_updates=True,
-                  pickled_dataset=BACH_DATASET, metadatas=None):
+def parallel_gibbs(models=None, melody=None, chorale_metas=None, sequence_length=50, num_iterations=1000,
+                   timesteps=16,
+                   model_base_name='models/raw_dataset/tmp/',
+                   temperature=1., initial_seq=None, batch_size_per_voice=16, parallel_updates=True,
+                   pickled_dataset=BACH_DATASET, metadatas=None):
     """
     samples from models in model_base_name
     """
@@ -736,16 +559,6 @@ def create_models(model_name=None, create_new=False, num_dense=200, num_units_ls
                              num_features_c=central_features.shape[-1],
                              num_pitches=num_pitches[voice_index],
                              num_dense=num_dense, num_units_lstm=num_units_lstm)
-        elif 'fastbach' in model_name:
-            model = fastBach(num_features_lr=left_features.shape[-1],
-                             num_features_c=central_features.shape[-1],
-                             num_pitches=num_pitches[voice_index],
-                             num_dense=num_dense, num_units_lstm=num_units_lstm)
-        elif 'skipnof' in model_name:
-            model = skip_nofermata(num_features_lr=left_features.shape[-1],
-                                   num_features_c=central_features.shape[-1],
-                                   num_pitches=num_pitches[voice_index],
-                                   num_dense=num_dense, num_units_lstm=num_units_lstm, timesteps=timesteps)
         elif 'skip' in model_name:
             model = skip(num_features_lr=left_features.shape[-1],
                          num_features_c=central_features.shape[-1],
@@ -914,8 +727,10 @@ def main():
                         type=int)
     args = parser.parse_args()
     print(args)
+
     # fixed set of metadatas to use
-    metadatas = [FermataMetadatas(), KeyMetadatas(windowSize=1)]
+    # metadatas = [FermataMetadatas(), KeyMetadatas(windowSize=1), TickMetadatas(SUBDIVISION), ModeMetadatas()]
+    metadatas = [TickMetadatas(), FermataMetadatas()]
 
     # datasets
 
