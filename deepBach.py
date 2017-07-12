a816c51c9b330a5271f560f92554f00075090d14
Konzert <HelloGitWorld@gmail.com>
2017-07-12 12:27:39 -0500

    Update deepBach.py

--- deepBach.py
+++ deepBach.py
@@ -823,7 +823,8 @@ def main():
 
     if not os.path.exists('models/' + model_name + '_' + str(NUM_VOICES - 1) + '.yaml'):
         create_models(model_name, create_new=overwrite, num_units_lstm=num_units_lstm, num_dense=num_dense,
-                      pickled_dataset=pickled_dataset, num_voices=num_voices, metadatas=metadatas, timesteps=timesteps)
+                      pickled_dataset=pickled_dataset, num_voices= 4 #  num_voices=num_voices CHANGED THIS LINE 
+                      , metadatas=metadatas, timesteps=timesteps)
     if train:
         models = train_models(model_name=model_name, steps_per_epoch=steps_per_epoch, num_epochs=num_epochs,
                               validation_steps=validation_steps, timesteps=timesteps, pickled_dataset=pickled_dataset,
